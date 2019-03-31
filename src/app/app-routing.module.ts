c6265bba8bd58258a9bba3435679c459197c4b18
Rohit Shrestha <srohitiw@gmail.com>
2019-03-31 21:41:40 +0545

    Initial Angular Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LazyModule} from './lazy/lazy.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'lazy',
+    loadChildren: './lazy/lazy.module#LazyModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
